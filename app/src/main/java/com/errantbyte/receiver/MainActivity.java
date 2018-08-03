1e73c809e11b736dad73c5dfb1e39bf05dce118d
errantbyte <errantbyte@outlook.com>
2018-08-03 23:18:20 +0300

    Added Toolbar and NavigationView

--- app/src/main/java/com/errantbyte/receiver/MainActivity.java
+++ app/src/main/java/com/errantbyte/receiver/MainActivity.java
@@ -1,12 +1,33 @@
 package com.errantbyte.receiver;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.support.v7.widget.Toolbar;
 
-public class MainActivity extends Activity{
+public class MainActivity extends AppCompatActivity {
+
+    private static final int LAYOUT = R.layout.main_activity;
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.ThemeDefault);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_activity);
+        setContentView(LAYOUT);
+
+        initToolbar();
+    }
+
+    private void initToolbar() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                return false;
+            }
+        });
+        toolbar.inflateMenu(R.menu.menu);
     }
 }
